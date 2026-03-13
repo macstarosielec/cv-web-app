82eb8dcf92accf65b7fe7dc9aa672cd0283039cf
Maciej Starosielec <maciej@code-snap.com>
2026-03-13 17:55:13 +0100

    app wirings

---
+++ apps/admin_app/lib/router/router.dart
@@ -0,0 +1,27 @@
+import 'package:admin_app/guards/auth_guard.dart';
+import 'package:admin_content/admin_content.dart';
+import 'package:auth/auth.dart';
+import 'package:auto_route/auto_route.dart';
+
+@AutoRouterConfig(
+  replaceInRouteName: 'Page,Route',
+)
+class AppRouter extends RootStackRouter {
+  @override
+  RouteType get defaultRouteType => const RouteType.material();
+
+  @override
+  List<AutoRoute> get routes => [
+    AutoRoute(page: LoginRoute.page),
+    AutoRoute(
+      page: DashboardRoute.page,
+      initial: true,
+      guards: [AuthGuard()],
+      children: [
+        AutoRoute(page: ProfileEditorRoute.page, initial: true),
+        AutoRoute(page: ProjectsListRoute.page),
+        AutoRoute(page: WorkExperienceListRoute.page),
+      ],
+    ),
+  ];
+}
